ee0cf06e8329295e640521edfdf8f6d438b69ef4
juno <selectjuno@gmail.com>
2021-03-25 23:43:17 +0900

    임시 사용자 추가

--- src/main/java/com/juno/securitybasesession/config/SecurityConfig.java
+++ src/main/java/com/juno/securitybasesession/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.juno.securitybasesession.config;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,5 +15,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication()
+                .withUser("user1")
+                .password("$2a$10$q7TxjtT9/CmBboxEHG5jmudD3lN98gZsc0NLtGxB.t6WUeRTTNPMO") // encode(1111)
+                .roles("USER");
     }
 }
