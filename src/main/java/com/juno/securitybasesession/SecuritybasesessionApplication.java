d85099ed0628bff4a6a6062363c393db24229dec
juno <selectjuno@gmail.com>
2021-03-25 23:11:33 +0900

    init

---
+++ src/main/java/com/juno/securitybasesession/SecuritybasesessionApplication.java
@@ -0,0 +1,13 @@
+package com.juno.securitybasesession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritybasesessionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecuritybasesessionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/juno/securitybasesession/SecuritybasesessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juno.securitybasesession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritybasesessionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
